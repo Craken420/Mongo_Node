0fafdd979d96a3421e54c1271e7ad75a43a93795
Luis Angel Peña <lapena@mavi.mx>
2021-04-06 11:47:41 -0500

    feat:: Add the Todo model to do querys in bd

--- src/app/bd/bd.js
+++ src/app/bd/bd.js
@@ -11,3 +11,11 @@ mongoose.connect(dbURI).then(
       console.log("Error connecting Database instance due to: ", err)
     }
 )
+
+// Model to doing querys in bd
+const TodoSchema = new mongoose.Schema({
+  name: String,
+  completed: Boolean,
+  note: String,
+  updated_at: { type: Date, default: Date.now },
+})
