a09a6e3e1105b263756a49245b50e17f9f366a24
Luis Angel Peña <lapena@mavi.mx>
2021-04-07 16:58:15 -0500

    feat:: Add callbacks in the controllers function

--- src/app/controllers/user-controller.js
+++ src/app/controllers/user-controller.js
@@ -4,29 +4,39 @@ const Todo = require('../models/task-model'),
 const UserCtrl = {}
 
 UserCtrl.getAllUsers = async function (req, res) {
-    let users = await User.find();
-    res.status(200).send(users)
+    await User.find( (err, users) => {
+        if (err) res.status(500).send(err.message);
+        else res.status(200).send(users)
+    })
 }
 
 UserCtrl.getUserByID = async function (req, res) {
-    let user = await User.findById(req.params.userId);
-    res.status(200).send(user)
+    await User.findById(req.params.userId, (err, user) => {
+        if (err) res.status(500).send(err.message);
+        else res.status(200).send(user)
+    })
 }
 
 UserCtrl.newUser = async function (req, res) {
     let user = await new User(req.body)
-    await user.save();
-    res.status(200).send({'message': 'User created!!!'})
+    await user.save((err, user) => {
+        if (err) res.status(500).send(err.message);
+        else res.status(200).send(user)
+    })
 }
 
 UserCtrl.updateUser = async function (req, res) {
-    await User.findByIdAndUpdate(req.params.userId, req.body)
-    res.status(200).send({'message': 'User Edited!!!'})
+    await User.findByIdAndUpdate(req.params.userId, req.body, (err) => {
+        if (err) res.status(500).send(err.message);
+        else res.status(200).send({'message': 'User Edited!!!'})
+    })
 }
 
 UserCtrl.deleteUser = async function (req, res) {
-    await User.findByIdAndRemove(req.params.userId)
-    res.status(200).send({'message': 'User Removed!!!'})
+    await User.findByIdAndRemove(req.params.userId, (err) => {
+        if (err) res.status(500).send(err.message);
+        else res.status(200).send({'message': 'User Removed!!!'})
+    })
 }
 
 UserCtrl.getUserTodo = async function (req, res) {
